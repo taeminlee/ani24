0b47aef68f0730cbcb4b972675a0d48e0579f6ce
Taemin Lee <persuade@gmail.com>
2020-01-31 20:35:09 +0900

    Update model.py

--- model.py
+++ model.py
@@ -46,4 +46,60 @@ class ModelSetting(db.Model):
             logger.error('Exception:%s %s', e, key)
             logger.error(traceback.format_exc())
 
+class ModelWavveEpisode(db.Model):
+    __tablename__ = 'plugin_%s_auto_episode' % package_name
+    __table_args__ = {'mysql_collate': 'utf8_general_ci'}
+    __bind_key__ = package_name
+
+    id = db.Column(db.Integer, primary_key=True)
+    contents_json = db.Column(db.JSON)
+    created_time = db.Column(db.DateTime)
+
+    programcode = db.Column(db.String)
+    
+    episodecode = db.Column(db.String)
+    
+    filename = db.Column(db.String)
+    duration = db.Column(db.Integer)
+    start_time = db.Column(db.DateTime)
+    end_time = db.Column(db.DateTime)
+    download_time = db.Column(db.Integer)
+    completed = db.Column(db.Boolean)
+    user_abort = db.Column(db.Boolean)
+    pf_abort = db.Column(db.Boolean)
+    etc_abort = db.Column(db.Integer) #ffmpeg 원인 1, 채널, 프로그램
+    ffmpeg_status = db.Column(db.Integer)
+    temp_path = db.Column(db.String)
+    save_path = db.Column(db.String)
+    pf = db.Column(db.Integer)
+    retry = db.Column(db.Integer)
+    filesize = db.Column(db.Integer)
+    filesize_str = db.Column(db.String)
+    download_speed = db.Column(db.String)
+    call = db.Column(db.String)
+
+    def __init__(self, call, info, streaming):
+        self.created_time = datetime.now()
+        self.completed = False
+        self.user_abort = False
+        self.pf_abort = False
+        self.etc_abort = 0
+        self.ffmpeg_status = -1
+        self.pf = 0
+        self.retry = 0
+        self.call = call
+        self.set_info(info)
+
+    def __repr__(self):
+        #return "<Episode(id:%s, episode_code:%s, quality:%s)>" % (self.id, self.episode_code, self.quality)
+        return repr(self.as_dict())
+
+    def as_dict(self):
+        ret = {x.name: getattr(self, x.name) for x in self.__table__.columns}
+        return ret         
+    
+    def set_info(self, data):
+        self.contents_json = data        
+        self.programcode = data['program_code']
+        self.episodecode = data['code']
 #########################################################
