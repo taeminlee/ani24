5f5d7e17b5dc3aadabf2b43e0179f7d3a785f665
Taemin Lee <persuade@gmail.com>
2020-02-02 23:46:46 +0900

    Update logic_ani24.py

--- logic_ani24.py
+++ logic_ani24.py
@@ -63,6 +63,7 @@ class LogicAni24(object):
                 return None
             url2 = 'https://fileiframe.com/ani_video4/%s.html?player=' % episode_id
             data = LogicAni24.get_html(url2)
+            logger.info(data)
             video_url = 'http%s.mp4' % data.split('.mp4"')[0].split('"http')[-1]
             return video_url
         except Exception as e:
